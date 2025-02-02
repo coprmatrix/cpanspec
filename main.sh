a8ea4151feedaea44087547392347c395a8bad3a
u <u>
2025-02-02 18:34:28 +0100

    update

--- main.sh
+++ main.sh
@@ -8,5 +8,5 @@ increment_version() {
 
 ver="$(cat cpanspec/cpanspec | sed -n 's/our[[:space:]]*$VERSION[[:space:]]*=[[:space:]]*'"'"'\(.*\)'"'"'.*/\1/p;')"
 ver="$(increment_version $ver 0 1)"
-ver="$(increment_version $ver 1 2)"
+ver="$(increment_version $ver 1 3)"
 sed -i "s/.*\(version:\).*/\1 $ver/" *.obsinfo
