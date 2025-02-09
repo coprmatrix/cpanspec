005c16dcca5098649b5ef16edf60726c9f6b249a
u <u>
2025-02-09 17:24:36 +0100

    update

--- main.sh
+++ main.sh
@@ -1,12 +1,14 @@
-increment_version() {
-        local delimiter=.;
-        local ver=${3:-1};
-        local array=($(echo "$1" | tr $delimiter '\n'));
-        array[$2]=$((array[$2]+$ver));
-        echo $(local IFS=$delimiter ; echo "${array[*]}");
-}
+#increment_version() {
+#        local delimiter=.;
+#        local ver=${3:-1};
+#        local array=($(echo "$1" | tr $delimiter '\n'));
+#        array[$2]=$((array[$2]+$ver));
+#        echo $(local IFS=$delimiter ; echo "${array[*]}");
+#}
 
 ver="$(cat cpanspec/cpanspec | sed -n 's/our[[:space:]]*$VERSION[[:space:]]*=[[:space:]]*'"'"'\(.*\)'"'"'.*/\1/p;')"
-ver="$(increment_version $ver 0 1)"
-ver="$(increment_version $ver 1 3)"
+
+#ver="$(increment_version $ver 0 1)"
+#ver="$(increment_version $ver 1 3)"
+
 sed -i "s/.*\(version:\).*/\1 $ver/" *.obsinfo
